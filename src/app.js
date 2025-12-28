5e9ace62aa190c64daec960a8e655dc1490174cc
kirushamatvieiev008 <mkiril087@gmail.com>
2025-12-28 16:01:34 +0200

    gh

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import football from "./football.json"
 import template from "./template.hbs"
+import newFootball from "./newFootball.json"
 
 console.log(template);
 
 const list = document.querySelector('.jsList');
 
-list.innerHTML = template({football})
+list.innerHTML = template({newFootball})
